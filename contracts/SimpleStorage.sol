d8ca2e8b2b8c72bf1f4e33641400beeaac54759a
VivekZala <vivekzala28@gmail.com>
2024-02-22 19:36:37 +0530

    Added SimpleStorage.sol

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT 
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    uint256 FavNumber;
+
+    struct Person {
+        string Name;
+        uint256 FavNumber;
+    }
+
+    Person[] public people;
+
+    mapping(string => uint256) indexing;
+
+    function store(uint256 _FavNumber) public {
+        FavNumber = _FavNumber;
+    }
+
+    function retrive() view public returns(uint256) {
+        return FavNumber;
+    }
+
+    function addPerson(string memory _Name, uint256 _FavNumber) public {
+        people.push(Person(_Name, _FavNumber));
+        indexing[_Name] = _FavNumber;
+    }
+
+    function findFavNumber(string memory _Name) view public returns(uint256) {
+        return indexing[_Name];
+    }
+}
